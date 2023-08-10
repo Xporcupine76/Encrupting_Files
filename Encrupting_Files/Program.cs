6e3de1cb2b5655f410872a0cafd4ea729b7db72b
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-10 13:24:41 +0200

    Add project files.

---
+++ Encrupting_Files/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Encrupting_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFile();
+            Console.WriteLine("press enter to encrypt the file");
+            Console.ReadLine();
+
+            EncryptFiles(@"c:\MLFiles\ml.txt");
+
+            Console.WriteLine("Press enter to decrypt the file ");
+            Console.ReadLine();
+
+            DecryptFile(@"c:\MLFiles\ml.txt");
+        }
+
+        public static void ReadFile()
+        {
+
+            int counter = 0;
+            string line;
+
+            //Read the file and display line by line
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"c:\MLFiles\ml.txt");
+            while ((line = file.ReadLine()) != null)
+                Console.WriteLine(line);
+            counter++;
+
+            file.Close();
+
+        }
+        public static void EncryptFiles(string x)
+        {
+
+            File.Encrypt(x);
+
+        }
+        public static void DecryptFile(string x)
+        {
+
+            File.Decrypt(x);
+
+        }
+    }
+}
